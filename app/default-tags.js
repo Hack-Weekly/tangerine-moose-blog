4492c0f1a3d7f99e5aa5d203b28892fed378c737
andy <aphanIT@protonmail.com>
2023-04-01 17:22:00 -0400

    Cleared workspace and updated header tag

---
+++ app/default-tags.js
@@ -0,0 +1,14 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <title>Tangerine Moose Blog App</title>
+      <meta
+        name="description"
+        content="Blog app engineered by the Tangerine Moose team"
+      />
+      <meta author="Tangerine Moose Team" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/moose.ico" />
+    </>
+  );
+}
