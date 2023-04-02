606f3d3236bf77d65e33a968e701d80a7f5c3f30
andy <aphanIT@protonmail.com>
2023-04-02 00:09:15 -0400

    Cleaned workspace and updated header

--- app/default-tags.js
+++ app/default-tags.js
@@ -2,6 +2,7 @@ export default function DefaultTags() {
   return (
     <>
       <title>Tangerine Moose Blog App</title>
+      <meta charset="utf-8" />
       <meta
         name="description"
         content="Blog app engineered by the Tangerine Moose team"
