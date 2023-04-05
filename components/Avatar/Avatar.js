6dc39ffeb841c960e8c36a57d7cf1e040fc76811
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-04-04 23:57:40 -0400

    set default avatar and loading animation

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 import styles from "./Avatar.module.css";
 
-export default function Avatar({ avatar, name }) {
+export default function Avatar({ avatar, name, loading = false }) {
+  // TODO: Upload a default avatar image
   return (
     <div className={styles.root}>
-      <Image src={avatar} alt={name} width={40} height={40} />
+      {loading ? (
+        <div className={styles.loading}></div>
+      ) : (
+        <Image src={avatar || "/default_avatar.svg"} alt={name || ""} width={40} height={40} />
+      )}
     </div>
   );
 }
