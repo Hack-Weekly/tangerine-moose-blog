5787ffa4c3753be74740f9576bdd29794ed6a78e
Hamzah Mansour <hamzahbmansour@gmail.com>
2023-04-07 14:17:15 -0700

    use api route to get blogs from db, display on homepage

--- app/page.js
+++ app/page.js
@@ -1,38 +1,15 @@
-import { getDocs, orderBy, query } from "firebase/firestore";
-
-import { userCollection } from "@/firebase/utils/userUtils";
-
-const fetchData = async () => {
-  // TODO: change this to fetch blog collection instead
-  const q = await query(userCollection, orderBy("createdAt"));
-
-  const querySnapshot = await getDocs(q);
-  const users = [];
-  querySnapshot.forEach((doc) => {
-    // TODO: delete once blog collection is created
-    const { uid, createdAt, displayName, email } = doc.data();
-
-    const anon = displayName[0] + displayName.slice(1).replace(/./g, "*");
-    const anonEmail = email.replace(/(.).+@(.+)/, "$1***@$2");
-    users.push({
-      uid,
-      displayName: anon,
-      email: anonEmail,
-      createdAt,
-    });
-  });
-
-  return users;
-};
+import useRetrieveBlogs from "./[blog]/hooks/useRetrieveBlogs";
 
 export default async function Home() {
-  const blogs = await fetchData();
+  const blogs = await useRetrieveBlogs();
 
-  return (
+  return blogs.length ? (
     <div>
+      Blogs:
+      {/* TODO: replace with BlogCard component */}
       {blogs.map((blog) => {
         return (
-          <div key={blog.uid}>
+          <div key={blog.mid}>
             <pre
               style={{
                 whiteSpace: "pre-wrap",
@@ -48,5 +25,7 @@ export default async function Home() {
       })}
       <div style={{ height: "1000px" }}></div>
     </div>
+  ) : (
+    <div>No blogs yet</div>
   );
 }
