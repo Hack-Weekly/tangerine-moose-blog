e599e731b8aacce77026b01b9064b9eb9635185c
Hamzah Mansour <hamzahbmansour@gmail.com>
2023-04-07 14:17:15 -0700

    use api route to get blogs from db, display on homepage

--- app/api/retrieveBlogs/route.js
+++ app/api/retrieveBlogs/route.js
@@ -8,10 +8,7 @@ export async function GET(request) {
   // TODO: we are pulling all blogs for now
   // TODO: may need to set up different endpoints for filters?
   const blogs = [];
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
-    blogs.push(data.id, data.authorName, data.blogName, data.dateCreated);
-  });
+  querySnapshot.forEach((doc) => blogs.push(doc.data()));
 
   return new Response(JSON.stringify({ status: "success", result: blogs }));
 }
