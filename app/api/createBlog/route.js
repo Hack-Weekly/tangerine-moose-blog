a7b3b6bb09a55ec6a498b0f58fa3411e4efb4221
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-04-10 13:51:43 -0400

    update ignore file

--- app/api/createBlog/route.js
+++ app/api/createBlog/route.js
@@ -1,64 +1,62 @@
 import { NextResponse } from "next/server";
-import { addDoc, collection, doc, getDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import slugify from "@sindresorhus/slugify";
+import { doc, getDoc } from "firebase/firestore";
 
-import { firestore as db } from "@/firebase/firebase";
+import { createBlog, updateBlog } from "@/firebase/utils/blogUtils";
+import { docToUser, updateUser, userCollection } from "@/firebase/utils/userUtils";
 import { verifyAuthSSR } from "@/lib/authSSR";
-
-// TODO: use db utils when PR is merged
-// TODO: rename field names and use serverTimestamp for timestamps
 
 // POST create new blog
 export async function POST(request) {
   const verifiedUid = await verifyAuthSSR(request);
-  const { uid, blogName } = await request.json();
+  const { uid, blogName, description } = await request.json();
 
+  // check if user is authenticated
   if (uid !== verifiedUid) {
     console.log("not auth");
     return NextResponse.json({ status: "error", message: "Not authenticated." }, { status: 401 });
   }
 
-  const userRef = doc(db, "users", uid);
+  const userRef = doc(userCollection, uid);
   const userSnap = await getDoc(userRef);
 
-  if (userSnap.exists()) {
-    const user = userSnap.data();
-    console.log("user: ", user);
-
-    // check if user already has a blog
-    if (user.blog) {
-      return new Response(JSON.stringify({ status: "error", message: "user already has blog" }));
-    } else {
-      const q = query(collection(db, "blogs"), where("blogName", "==", blogName));
-
-      const querySnapshot = await getDocs(q);
-      console.log("querySnapshot.empty: ", querySnapshot.empty);
-      // check if blog by that name already exists
-      if (querySnapshot.empty) {
-        // generate blogId
-        const blogId = doc(collection(db, "blogs")).id;
-        console.log("blogId: ", blogId);
-
-        // add new blog to blogs collection
-        await addDoc(collection(db, "blogs"), {
-          id: blogId,
-          blogName: blogName,
-          authorName: user.displayName,
-          dateCreated: new Date().toISOString(),
-        });
-
-        // add name of new blog to user
-        await updateDoc(userRef, {
-          blog: blogName,
-        });
-
-        return new Response(JSON.stringify({ status: "success", blogId: blogId }));
-      } else {
-        // return error if blogName is already taken
-        return new Response(JSON.stringify({ status: "error", message: "blog already exists" }));
-      }
-    }
-  } else {
+  // check if user exists
+  if (!userSnap.exists()) {
     console.log("user doesn't exist");
-    return new Response(JSON.stringify({ status: "error", message: "user not found" }));
+    return NextResponse.json({ status: "error", message: "user doesn't exist." }, { status: 400 });
+  }
+
+  // check if user already has a blog
+  const user = docToUser(userSnap);
+  if (user.blogId) {
+    return NextResponse.json({ status: "error", message: "user already has blog" }, { status: 400 });
+  }
+
+  try {
+    // add new blog to blogs collection
+    console.log("creating blog");
+    const blogRef = await createBlog({
+      name: blogName.trim(),
+      description: description ? description.trim() : "",
+      userId: user.uid,
+    });
+
+    // add slug to blog
+    const slug = slugify(`${blogName}-${blogRef.id.substring(0, 5)}`);
+    await updateBlog(blogRef.id, {
+      slug: slug,
+    });
+    // add blog id to user
+    await updateUser(uid, {
+      blog: blogName.trim(),
+      blogId: blogRef.id,
+      blogSlug: slug,
+      blogDescription: description ? description.trim() : "",
+    });
+
+    return new Response(JSON.stringify({ status: "success", blogId: blogRef.id }));
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ status: "error", message: "Something went wrong." }, { status: 500 });
   }
 }
