e857ed671308cd8754065c1c4ba6946b3239aaf6
andy <aphanIT@protonmail.com>
2023-04-11 23:57:52 -0400

    andy/updated authorCard

--- components/Featured/Featured.js
+++ components/Featured/Featured.js
@@ -1,28 +1,34 @@
-import kid from "../../public/kid.png";
+import { doc, getDoc, getDocs, limit, orderBy, query } from "firebase/firestore";
+
+import { docToPost, postCollection } from "@/firebase/utils/postUtils";
+import { docToUser, userCollection } from "@/firebase/utils/userUtils";
 import AuthorCard from "../AuthorCard/AuthorCard";
 import PostCard from "../PostCard/PostCard";
 import styles from "./Featured.module.css";
 
-// mock user data
-const fetchMockData = (slug) => ({
-  id: 1234,
-  authorAvatar: kid,
-  displayName: "trinh andy",
-  blog: "Bob's Law Blog",
-  email: "admin@trinhandandy.com",
-  authorDescription:
-    "When the tangerine moose first heard about the concept of 'orange is the new black,' he couldn't help but feel a little personally offended.",
-  authorLinks: ["x", "y", "z"],
-});
-const data = fetchMockData();
-// mock end
+// get latest post
+const fetchPost = async () => {
+  const postQuery = await query(postCollection, orderBy("createdAt"), limit(1));
+  const postDoc = (await getDocs(postQuery)).docs;
+  return docToPost(postDoc[0]);
+};
 
-const Featured = (props) => {
+// get user
+const fetchUser = async (userId) => {
+  const userRef = await doc(userCollection, userId);
+  const userDoc = await getDoc(userRef);
+  return docToUser(userDoc);
+};
+
+const Featured = async (props) => {
+  const article = await fetchPost();
+  const user = await fetchUser(article.userId);
+
   return (
     <div className={styles.root}>
       <h2>FEATURED ARTICLE</h2>
-      <PostCard />
-      <AuthorCard {...data} futureStyle={styles.author} />
+      <PostCard {...article} {...user} />
+      <AuthorCard futureStyle={styles.author} />
     </div>
   );
 };
--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -1,46 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import stockPhoto from "@/public/StockPhotos/stockPhoto";
 import styles from "./PostCard.module.css";
 
-const mockProps = () => ({
-  id: 1234,
-  blogName: "blogName",
-  authorName: "authorName",
-  title: "The Awesome Title",
-  slug: "slug",
-  text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  createdAt: "August 11, 2000",
-});
-const data = mockProps();
-
 const PostCard = (props) => {
-  const { blogName, authorName, title, text, createdAt } = data;
   const photo = stockPhoto();
 
-  // TODO: take a substring of text
   const handleText = () => {
-    const result = text.substring(0, 250);
+    if (props.text == null) return;
+    const result = props.text.substring(0, 250);
     return result + "...";
   };
 
+  const handleTags = () => {
+    const result = props.tags.slice(0, 3);
+    return result.map((tag) => <span key={tag}>{tag}</span>);
+  };
+
+  const options = { month: "2-digit", day: "2-digit", year: "numeric" };
+  const formattedDate = new Date(props.createdAt.seconds * 1000).toLocaleDateString(undefined, options);
+
   return (
-    <div className={`${styles.root} ${styles.futureStyle}`}>
+    <Link href={`/${props.bodySlug}/${props.slug}`} className={`${styles.root} ${styles.futureStyle}`}>
       <Image src={photo} alt="" width={275} height={366} className={styles.image} />
       <fieldset className={styles.container}>
         <legend className={styles.legend}>article</legend>
         {/* TODO: implement tags */}
-        <div className={styles.tags}>tags</div>
-        <div className={styles.title}>{title}</div>
+        <div className={styles.tags}>{handleTags()}</div>
+        <div className={styles.title}>{props.title}</div>
         <div className={styles.info}>
-          <div>{authorName}</div>
+          <div>{props.displayName}</div>
           <span>&#8226;</span>
           {/* TODO: format date */}
-          <div>{createdAt}</div>
+          <div>{formattedDate}</div>
         </div>
         <div className={styles.content}>{handleText()}</div>
       </fieldset>
-    </div>
+    </Link>
   );
 };
 
